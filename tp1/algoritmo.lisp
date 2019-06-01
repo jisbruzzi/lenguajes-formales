3696a8e3f4caf315a0b8e13f13dac20fa2af84a1
jisbruzzi <jose.sbru@gmail.com>
2019-06-01 15:19:08 -0300

    Reorganizo el codigo

---
+++ tp1/algoritmo.lisp
@@ -0,0 +1,59 @@
+; ------------------------
+(load 'generalidades)
+(load 'primitivas)
+
+
+; ----------------- parte abstracta, independiente de la implementación de los tipos de dato ------------
+(defun posibles_vecinos (grafo trayectoria)
+  (diferencia
+    (vecinos (ultimo_vertice trayectoria) grafo)
+    (vertices_de_trayectoria trayectoria)
+  )
+)
+
+(defun expandir_trayectoria (grafo trayectoria)
+  (mapcar
+    (lambda (v) (construir_trayectoria_hacia_vertice v trayectoria))
+    (posibles_vecinos grafo trayectoria)
+  )
+)
+
+(defun expandir_trayectorias (grafo trayectorias)
+  (apply 
+    'append 
+    (mapcar 
+      (lambda (tr) (expandir_trayectoria grafo tr)) 
+      trayectorias
+    )
+  )
+)
+
+
+
+(defun trayectoria_que_alcanza (nodo trayectorias)
+  (car 
+    (filtrar 
+      (lambda (tr) (trayectoria_alcanza tr nodo ) )
+      trayectorias
+    )
+  )
+)
+
+
+
+(defun existe_trayectoria_que_alcanza (nodo trayectorias)
+  (not (null 
+    (trayectoria_que_alcanza nodo trayectorias)
+  ))
+)
+
+
+
+(defun GPS (inicio fin grafo &optional (trayectorias (list (list inicio))))
+  (cond 
+    ( (null trayectorias) nil )
+    ( (existe_trayectoria_que_alcanza fin trayectorias) (trayectoria_que_alcanza fin trayectorias) )
+    ( T (GPS inicio fin grafo (expandir_trayectorias grafo trayectorias)))
+  )
+)
+
---
+++ tp1/pruebas-algoritmo.lisp
@@ -0,0 +1,70 @@
+(load 'mapa)
+(load 'generalidades)
+(load 'primitivas)
+(load 'algoritmo)
+
+(defun test (elemento resultado)
+    (if (equal elemento resultado)
+        (print "ok")
+        (print (list "error:" elemento " es distinto de " resultado))
+    )
+)
+
+
+(test (filtrar (lambda (x) (eq x 1) ) '(1 2 3 4 5 1 1 1))  '(1 1 1 1)  )
+
+(test (vecinos 'a grafo) '(b f))
+(test (vecinos 'b grafo) '(a c))
+(test (vecinos 'i grafo) '(m j))
+
+
+
+(test (posibles_vecinos grafo '(a b c)) '(f))
+
+(test (posibles_vecinos grafo '(n d c b a)) '(j m))
+
+
+
+(test 
+  (expandir_trayectoria grafo '(d c b a))
+  '((n d c b a) (e d c b a))
+)
+
+(test
+  (expandir_trayectoria grafo '(n d c b a))
+  '((J N D C B A) (M N D C B A))
+)
+
+(test
+  (expandir_trayectoria grafo '(e d c b a))
+  nil
+)
+
+(test
+  (expandir_trayectorias grafo '((n d c b a) (e d c b a)) )
+  '((J N D C B A) (M N D C B A))
+)
+
+(test
+  (trayectoria_que_alcanza 'n '((n d c b a) (e d c b a)) )
+  '(n d c b a)
+)
+
+(test
+  (existe_trayectoria_que_alcanza 'n '((n d c b a) (e d c b a)) )
+  t
+)
+
+
+(test (GPS 'a 'b grafo) '(b a))
+(test (GPS 'a 'd grafo) '(d c b a))
+
+(test
+  (GPS 'a 'l grafo)
+  '(L H G F A)
+)
+
+(test 
+  (GPS 'a 'k grafo)
+  '(K J N D C B A)
+)
