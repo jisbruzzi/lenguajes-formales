92edeebb99d8f8a14cd1c1b949110c96fb7803a3
jisbruzzi <jose.sbru@gmail.com>
2019-06-01 15:01:23 -0300

    Terminada la busqueda de caminos

--- tp1v2.lisp
+++ tp1v2.lisp
@@ -24,9 +24,52 @@
 
 
 ;---------------- parte con primitivas usuales -------------------
-(defun pertenece (e l)
-  
+
+(defun filtrar (f L)
+  (reduce
+    (lambda (x y) 
+      (if (funcall f y)
+        (append x (list y))
+        x
+      )
+    )
+    (cons nil L)
+  )
 )
+
+(assert (equal (filtrar (lambda (x) (eq x 1) ) '(1 2 3 4 5 1 1 1))  '(1 1 1 1)  ))
+
+(defun pertenece (e l)
+  (reduce 
+    (lambda (x y) (or x y))
+    (mapcar 
+      (lambda (x) (eq e x))
+      l
+    )
+  )
+)
+
+(defun diferencia (base quitar)
+  (cond
+    (
+      (null base) 
+      nil
+    )
+    (
+      (pertenece (car base) quitar) 
+      (diferencia (cdr base) quitar) 
+    )
+    (
+      T 
+      (cons
+        (car base)
+        (diferencia (cdr base) quitar)
+      )
+    )
+  )
+)
+
+; ------------------------
 
 ;la trayectoria va a ser una lista de vértices (nombre "código")
 ;el primer elemento de ña trayectoria es el "fin", el último elemento de la trayectoria es el "inicio"
@@ -56,7 +99,7 @@
 
 ;implementación
 (defun vertices_de_trayectoria (trayectoria)
-  treyectoria
+  trayectoria
 )
 
 ;implementación
@@ -79,6 +122,10 @@
 (assert (equal (vecinos 'b grafo) '(a c)))
 (assert (equal (vecinos 'i grafo) '(m j)))
 
+(defun ultimo_vertice (trayectoria)
+  (car trayectoria)
+)
+
 
 ; ----------------- parte abstracta, independiente de la implementación de los tipos de dato ------------
 (defun posibles_vecinos (grafo trayectoria)
@@ -87,28 +134,63 @@
     (vertices_de_trayectoria trayectoria)
   )
 )
+(assert (equal (posibles_vecinos grafo '(a b c)) '(f)))
 
-(print (posibles_vecinos grafo '(a b c)))
+(assert
+  (equal (posibles_vecinos grafo '(n d c b a)) '(j m))
+)
 
-(defun expandir_trayectoria (inicio fin grafo trayectoria)
+(defun expandir_trayectoria (grafo trayectoria)
   (mapcar
     (lambda (v) (construir_trayectoria_hacia_vertice v trayectoria))
     (posibles_vecinos grafo trayectoria)
   )
 )
 
-(defun expandir_trayectorias (inicio fin grafo trayectorias)
-  (apply 'append (mapcar (lambda (t) (expandir_trayectoria grafo t)) trayectorias))
-)
+(assert (equal 
+  (expandir_trayectoria grafo '(d c b a))
+  '((n d c b a) (e d c b a))
+))
 
-(defun trayectoria_que_alcanza (nodo trayectorias)
-  (car 
-    (filtrar 
-      (lambda (t) (trayectoria_alcanza t nodo ) )
+(assert (equal
+  (expandir_trayectoria grafo '(n d c b a))
+  '((J N D C B A) (M N D C B A))
+))
+
+(assert (equal
+  (expandir_trayectoria grafo '(e d c b a))
+  nil
+))
+
+
+(defun expandir_trayectorias (grafo trayectorias)
+  (apply 
+    'append 
+    (mapcar 
+      (lambda (tr) (expandir_trayectoria grafo tr)) 
       trayectorias
     )
   )
 )
+
+(assert (equal
+  (expandir_trayectorias grafo '((n d c b a) (e d c b a)) )
+  '((J N D C B A) (M N D C B A))
+))
+
+(defun trayectoria_que_alcanza (nodo trayectorias)
+  (car 
+    (filtrar 
+      (lambda (tr) (trayectoria_alcanza tr nodo ) )
+      trayectorias
+    )
+  )
+)
+
+(assert (equal
+  (trayectoria_que_alcanza 'n '((n d c b a) (e d c b a)) )
+  '(n d c b a)
+))
 
 (defun existe_trayectoria_que_alcanza (nodo trayectorias)
   (not (null 
@@ -116,11 +198,29 @@
   ))
 )
 
+(assert (equal
+  (existe_trayectoria_que_alcanza 'n '((n d c b a) (e d c b a)) )
+  t
+))
 
-(defun GPS (inicio fin grafo &optional (trayectorias (list (list i))))
+
+(defun GPS (inicio fin grafo &optional (trayectorias (list (list inicio))))
   (cond 
     ( (null trayectorias) nil )
     ( (existe_trayectoria_que_alcanza fin trayectorias) (trayectoria_que_alcanza fin trayectorias) )
-    ( T (GPS inicio fin grafo (expandir_trayectorias inicio fin grafo trayectorias)))
+    ( T (GPS inicio fin grafo (expandir_trayectorias grafo trayectorias)))
   )
 )
+
+(assert (equal (GPS 'a 'b grafo) '(b a)))
+(assert (equal (GPS 'a 'd grafo) '(d c b a)))
+
+(assert (equal
+  (GPS 'a 'l grafo)
+  '(L H G F A)
+))
+
+(assert (equal 
+  (GPS 'a 'k grafo) 
+  '(K J N D C B A)
+))
