1e02a4976090aff1bdbe1f1c1f452bc8d94ddc58
jisbruzzi <jose.sbru@gmail.com>
2019-06-01 16:22:05 -0300

    Agrego la interfaz de entrada

--- tp1/interface.lisp
+++ tp1/interface.lisp
@@ -1,17 +1,7 @@
 (load 'mapa)
 (load 'algoritmo)
+(load 'herramientasInterface)
 
-(defun calle_valida (calle)
-  (pertenece (print calle )
-    (reduce
-      'append
-      (mapcar 
-        (lambda (x) (cadr x))
-        diccionario
-      )
-    )
-  )
-)
 
 (defun obtenerCalleValida ()
   (let
@@ -27,23 +17,6 @@
   )
 )
 
-(defun misma_esquina (e1 e2)
-  (and
-    (eq (car e1) (car e2))
-    (eq (cadr e1) (cadr e2))
-  )
-)
-
-(defun vertice_de_esquina (esq &optional (dic diccionario))
-  (cond 
-    ( (null dic) nil)
-    ( (misma_esquina esq (cadar dic)) (caar dic))
-    (T (vertice_de_esquina esq (cdr dic)))
-  )
-)
-(defun esquina_valida (esq)
-  (not (null (vertice_de_esquina esq)))
-)
 
 
 (defun obtenerEsquina ()
