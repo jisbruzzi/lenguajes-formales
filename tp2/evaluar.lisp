77ce19c49242b5de1a4e85594516618dea262578
jisbruzzi <jose.sbru@gmail.com>
2019-06-06 17:29:46 -0300

    Menos archivos en la parte del tp2

--- tp2/evaluar.lisp
+++ tp2/evaluar.lisp
@@ -1,6 +1,107 @@
-(load 'control_flujo)
-(load 'conocidas)
+; ---------- PRIMITIVAS DE DICCIONARIO ----- ;
+(defun en_diccionario (diccionario clave)
+  (not (null (buscar_diccionario diccionario clave)))
+)
+(defun buscar_diccionario (diccionario clave)
+  (cond
+    ((null diccionario) nil)
+    ((eq (caar diccionario) clave) (cadar diccionario))
+    (T (buscar_diccionario (cdr diccionario) clave))
+  )
+)
 
+; ---------- DICCIONARIO DE FUNCIONES CONOCIDAS QUE CONTROLAN EL FLUJO DEL PROGRAMA ----- ;
+(defmacro l (nombre form)
+  `(list (quote ,nombre) (lambda (expr evfn) ,form))
+)
+(defmacro ev (form)
+  `(funcall evfn ,form)
+)
+
+(setq control_flujo
+  (list
+    (l quote (cadr expr))
+    (l and 
+      (if (null (ev (cadr expr)))
+        nil
+        (ev (caddr expr))
+      )
+    )
+    (l or
+      (if (null (ev (cadr expr)))
+        (ev (caddr expr))
+        (ev (cadr expr))
+      )
+    )
+    (l if
+      (if (not (null (ev (cadr expr)) ))
+        (ev (caddr expr))
+        (ev (cadddr expr))
+      )
+    )
+  )
+)
+
+(defun es_control_flujo (nombre)
+    (en_diccionario control_flujo nombre)
+)
+
+(defun funcion_control_flujo (nombre)
+    (buscar_diccionario control_flujo nombre)
+)
+
+; --------- DICCIONARIO DE FUNCIONES CONOCIDAS --------- ;
+(defmacro cf (nombre)
+  `(list 
+    (quote ,nombre)
+    (lambda (argumentos evfn)
+      (apply ',nombre argumentos)
+    )
+  )
+)
+
+(setq conocidas
+  (list
+    (cf car)
+    (cf cdr)
+    (cf list)
+    (cf *)
+    (cf +)
+    (cf -)
+    (cf cons)
+    (cf numberp)
+    (cf eq)
+    (list 
+      (quote mapcar)
+      (lambda (args evfn)
+        (apply 
+          'mapcar 
+          (cons
+            (lambda (&rest elementos)
+              (funcall evfn
+                (cons 
+                  (car args) 
+                  (mapcar (lambda (e) (list `quote e)) elementos)
+                )
+              )
+            )
+            (cdr args)
+          )
+        )
+      )
+    )
+  )
+)
+
+(defun es_conocida (nombre)
+  (en_diccionario conocidas nombre)
+)
+
+(defun funcion_conocida (nombre)
+  (buscar_diccionario conocidas nombre)
+)
+
+; ------ DEFINICIÓN DE LA FUNCIÓN EVALUAR ---- ;
 (defun pertenece (e l)
   (reduce 
     (lambda (x y) (or x y))
